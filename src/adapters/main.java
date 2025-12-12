d7a534b975f49a96ca7f34253076c80bf58b3161
quoc.nguyen <nguyen.quocdat.1029@gmail.com>
2025-12-12 21:11:24 +0100

    rangement

--- src/adapters/main.java
+++ src/adapters/main.java
@@ -2,7 +2,7 @@ package src.adapters;
 
 import src.entities.Board.Board;
 
-public class main {
+public class main {  // utilise uci et le board seulement je crois ou faire une interface board pour ensuite lutiliser
     public static void main(String[] args) {
         Board board = new Board();
         System.out.println(board.getBoard());
---
+++ src/useCases/Uci.java
@@ -0,0 +1,4 @@
+package src.useCases;
+
+public class Uci { // fait avec chatgpt afin de creer un uci si on na pas de temps
+}
